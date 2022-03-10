3928e80112d2ccb017e3ee7b6956d9f0f46884ea
Hazmat11 <ebertrand@gaming.tech>
2022-03-10 15:00:55 +0100

    ça marche pas

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { Container } from 'react-bootstrap';
-import MyNavBar from './component/menu/Navbar.js';
+import { Container, Navbar } from 'react-bootstrap';
+import MyNavBar from './component/menu/MyNavBar.js';
 import ArticlesCards from './component/articles/ArticlesCards.js';
 
 function App() {
   return (
     <div className="App">
-      <MyNavBar />
+      <Navbar />
       <p><br></br></p>
       <h1>Exotic Taste</h1>
       <h2>The taste of rarity</h2>
